15b778265ee390f64b6d3eb67af65a35b7bb34a0
fecet <xiezej@gmail.com>
2021-08-10 10:59:41 +0800

    dracula

--- config.h
+++ config.h
@@ -4,8 +4,8 @@ static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "#000000",   /* after initialization */
-	[INPUT] =  "#282c34",   /* during input */
-	[FAILED] = "#be5046",   /* wrong password */
+	[INPUT] =  "#282a36",   /* during input */
+	[FAILED] = "#ff5555",   /* wrong password */
 };
 
 /* lock screen opacity */
@@ -15,10 +15,11 @@ static const float alpha = 0.5;
 static const int failonclear = 1;
 
 /* default message */
-static const char * message = "Enter password to unlock";
+static const char * message = "None are more hopelessly enslaved than those who falsely believe they are free";
+//static const char * message = "Greatness, at any cost";
 
 /* text color */
-static const char * text_color = "#abb2bf";
+static const char * text_color = "#bd93f9";
 
 /* text size (must be a valid size) */
-static const char * text_size = "fixed";
+static const char * text_size = "lucidasanstypewriter-bold-24";
